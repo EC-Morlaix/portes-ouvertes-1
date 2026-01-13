976b7373601f4c38ad6f1ae39a50f0e1120fc436
dnunez-gua <52534681+dnunez-gua@users.noreply.github.com>
2026-01-13 14:01:47 +0100

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,12 @@
+// ui.js
+function loadStands() {
+  stands.forEach(stand => {
+    L.polygon(stand.coords, {
+      color: 'green',
+      fillOpacity: 0.5
+    })
+    .addTo(map)
+    .bindPopup(`<b>${stand.nom}</b><br>${stand.exposant}`);
+  });
+}
 
