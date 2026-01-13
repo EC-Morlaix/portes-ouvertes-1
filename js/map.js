0e645f9af77c791508f06f78b20aa915067a1068
dnunez-gua <52534681+dnunez-gua@users.noreply.github.com>
2026-01-13 14:00:03 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,1 +1,11 @@
+// map.js
+let map;
+
+function initMap() {
+   map = L.map('map').setView([48.576, -3.836], 20);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap'
+  }).addTo(map);
+}
 
