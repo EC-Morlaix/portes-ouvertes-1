2e43c9b9174f76c316e5c965fb55f86f1acdee4b
dnunez-gua <52534681+dnunez-gua@users.noreply.github.com>
2026-01-14 17:43:14 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,11 +1,16 @@
-// map.js
-let map;
+// Dimensions réelles de l’image (en pixels)
+const imageWidth = 2000;
+const imageHeight = 1200;
 
-function initMap() {
-   map = L.map('map').setView([48.576, -3.836], 18);
+// Coordonnées fictives
+const bounds = [[0, 0], [imageHeight, imageWidth]];
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap'
-  }).addTo(map);
-}
+const map = L.map('map', {
+  crs: L.CRS.Simple,
+  minZoom: -2,
+  maxZoom: 2
+});
 
+L.imageOverlay('img/10rdc.PNG', bounds).addTo(map);
+
+map.fitBounds(bounds);
