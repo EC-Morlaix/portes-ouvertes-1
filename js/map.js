fe496d2d47b4414b6601fcc7f2776a5af26afe97
dnunez-gua <52534681+dnunez-gua@users.noreply.github.com>
2026-01-14 20:31:25 +0100

    Add files via upload

---
+++ js/map.js
@@ -0,0 +1,20 @@
+// Dimensions réelles de l’image (en pixels)
+const imageWidth = 1505;
+const imageHeight = 593;
+
+// Coordonnées fictives
+const bounds = [[0, 0], [imageHeight, imageWidth]];
+
+const map = L.map('map', {
+  crs: L.CRS.Simple,
+  minZoom: 0,
+  maxZoom: 2
+});
+
+L.imageOverlay('img/10rdc.PNG', bounds).addTo(map);
+
+map.fitBounds(bounds);
+setTimeout(() => {
+  map.invalidateSize();
+}, 300);
+
