b42525979c0f2b29b68b8191e8c8b0b61a81ed21
dnunez-gua <52534681+dnunez-gua@users.noreply.github.com>
2026-01-14 19:55:09 +0100

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -8,15 +8,17 @@ function loadStands() {
     const height = 30;  // hauteur en "unités image"
 
     const bounds = [
-      [y - height / 2, x - width / 2],
-      [y + height / 2, x + width / 2]
+     // [y - height / 2, x - width / 2],
+      [y + height / 2, x + width / 2]//
+       [y - height , x - width ],
+      [y + height , x + width ]
     ];
 
     L.rectangle(bounds, {
       color: '#2e7d32',
       weight: 1,
       fillColor: '#4caf50',
-      fillOpacity: 0.7
+      fillOpacity: 0.9
     })
     .addTo(map)
     .bindPopup(`<b>${stand.nom}</b><br>${stand.exposant}<br><a href=${stand.info}>site web</a>`);
