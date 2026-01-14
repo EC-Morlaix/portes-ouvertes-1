5f27b7fde08f7af13d244c317d3181ce04ec168a
dnunez-gua <52534681+dnunez-gua@users.noreply.github.com>
2026-01-14 14:52:33 +0100

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,9 +1,15 @@
 // ui.js
 function loadStands() {
   stands.forEach(stand => {
-    L.marker(stand.coords)
+    const iconStand = L.divIcon({
+      className: "stand-rect",
+      html: `<div>${stand.nom}</div>`,
+      iconSize: [60, 30],
+      iconAnchor: [30, 15]
+    });
+
+    L.marker(stand.coords, { icon: iconStand })
       .addTo(map)
       .bindPopup(`<b>${stand.nom}</b><br>${stand.exposant}`);
   });
 }
-
