4a817951fee7ca8257ff1e86e916344300d7d65f
dnunez-gua <52534681+dnunez-gua@users.noreply.github.com>
2026-01-17 15:28:25 +0100

    Update data-stands.js

--- js/data-stands.js
+++ js/data-stands.js
@@ -2,7 +2,7 @@ const stands = [
   {
     nom: "10-01",
     exposant: "Mobilités ERASMUS+",
-    coords: [1280,248,80,60], // y, x en pixels,
+    coords: [1285,248,80,61], // y, x en pixels,
     info:"https://www.ecmorlaix.fr/",
     color: '#4caf50',
   }
