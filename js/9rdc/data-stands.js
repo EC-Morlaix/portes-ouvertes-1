80fbd1a95c84ea0becc61a3b3a81942735a90491
Delphine Nunez <delphine.nunez@ecmorlaix.fr>
2026-02-24 11:56:22 +0100

    markeur

--- js/9rdc/data-stands.js
+++ js/9rdc/data-stands.js
@@ -1,4 +1,13 @@
 const stands = [
-
+ {atelier:false,
+    pro:false,
+    gt:false,
+    cursus : "Aller vers les batiments 5 et 10",
+    nom: "parc",
+    exposant: "Lycée Professionnel - Lycée Général et Technologique",
+    coords: [485,200], // y, x en pixels,
+    titreInfo:"Accéder",
+    info:"../doc/plan10rdc.html",
+  }
 ];
 
